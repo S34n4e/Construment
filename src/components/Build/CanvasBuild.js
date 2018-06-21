214889e4a11934ef61a88d122457cb2286f7004e
metaldeveloperx2 <metaldeveloperx2@gmail.com>
2018-06-21 21:28:11 +0000

    actualizacion con la cosa volcada y fea

--- src/components/Build/CanvasBuild.js
+++ src/components/Build/CanvasBuild.js
@@ -1,15 +1,7 @@
 import React, { Component }	from 'react';
-//import guitarra from "../../Multimedia/Guitarras.jpg";
-//import mergeImages from 'merge-images';
 
 const data = require('./data.json'); const parts = require('./parts.json');
-var acc = document.getElementsByClassName("accordion"); var i;
-var canvasStyle = {border: '1px solid #d3d3d3'}; var hideStyle = {display:'none'}; 
-var x1 = 137; var y1 = 180;
-var x2 = 200; var y2 = 40;
-var x3 = 200; var y3 = 0;
-var canvas; var ctx; 
-var partEditing = "body";
+var acc = document.getElementsByClassName("accordion"); var i; var hideStyle = {display:'none'};
 var imgBody; var imgNeck; var imgPeg; var indentsBody = []; var indentsNeck = []; var indentsPeg = [];
 
 
@@ -40,122 +32,10 @@ function clearContent(){
 }
 
 function start() {
-     canvas = document.getElementById("myCanvas");
-     ctx = canvas.getContext("2d");
-     imgBody = document.getElementById("imgBody");
-     imgNeck = document.getElementById("imgNeck");
-     imgPeg = document.getElementById("imgPeg");
-        if (canvas.getContext) {
-          ctx = canvas.getContext("2d");
-          window.addEventListener('resize', resizeCanvas, false);
-          window.addEventListener('orientationchange', resizeCanvas, false);
-          resizeCanvas();
-        x1 = canvas.width/2 - imgBody.naturalWidth/4;
-        x2 = canvas.width/2 - imgNeck.naturalWidth/4;
-        x3 = canvas.width/2 - imgPeg.naturalWidth/4;
-          ctx.drawImage(imgBody, x1, y1, imgBody.naturalWidth/2, imgBody.naturalHeight/2);
-          ctx.drawImage(imgNeck, x2, y2, imgNeck.naturalWidth/2, imgNeck.naturalHeight/2);
-          ctx.drawImage(imgPeg, x3, y3, imgPeg.naturalWidth/2, imgPeg.naturalHeight/2);
-            
-        }
-   console.log("cargado");
+    imgBody = document.getElementById("imgBody");
+    imgNeck = document.getElementById("imgNeck");
+    imgPeg = document.getElementById("imgPeg");
    leftPanel();
-};
-
-function resizeCanvas() {
-        canvas.width = Math.max(window.innerWidth/2, 290);
-        canvas.height = Math.max(window.innerHeight/2, 400);
-        x1 = canvas.width/2 - imgBody.naturalWidth/4;
-            x2 = canvas.width/2 - imgNeck.naturalWidth/4;
-            x3 = canvas.width/2 - imgPeg.naturalWidth/4;
-        addImage();
-      }
-
-function changePart(){
-    if(partEditing === "body") {
-        partEditing = "neck";
-    } else if (partEditing === "neck") {
-        partEditing = "peg";
-    } else if (partEditing === "peg") {
-        partEditing = "body";
-    }
-    document.getElementById("editChange").textContent = "Move: " + partEditing;
-}
-
-function addImage() {
-    const context = canvas.getContext("2d");
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(imgBody, x1, y1, imgBody.naturalWidth/2, imgBody.naturalHeight/2);
-    ctx.drawImage(imgNeck, x2, y2, imgNeck.naturalWidth/2, imgNeck.naturalHeight/2);
-    ctx.drawImage(imgPeg, x3, y3, imgPeg.naturalWidth/2, imgPeg.naturalHeight/2);
-}
-
-function upX(){
-    switch(partEditing){
-        case "body":
-            x1 = x1+1;
-            break;
-        case "neck":
-            x2 = x2+1;
-            break;
-        case "peg":
-            x3 = x3+1;
-            break;
-        default:
-            break;
-    }
-    addImage();
-}
-
-function upY(){
-    switch(partEditing){
-        case "body":
-            y1 = y1+1;
-            break;
-        case "neck":
-            y2 = y2+1;
-            break;
-        case "peg":
-            y3 = y3+1;
-            break;
-        default:
-            break;
-    }
-    addImage();
-}
-
-function downX(){
-    switch(partEditing){
-        case "body":
-            x1 = x1-1;
-            break;
-        case "neck":
-            x2 = x2-1;
-            break;
-        case "peg":
-            x3 = x3-1;
-            break;
-        default:
-            break;
-    }
-    addImage();
-}
-
-function downY(){
-    switch(partEditing){
-        case "body":
-            y1 = y1-1;
-            break;
-        case "neck":
-            y2 = y2-1;
-            break;
-        case "peg":
-            y3 = y3-1;
-            break;
-        default:
-            break;
-    }
-    addImage();
 }
 
 function changeImg(num, instrum){
@@ -172,39 +52,164 @@ function changeImg(num, instrum){
         default:
             break;
     }
-    addImage();
 }
 
 function elements() {
-    
     if(data.none.name === "guitar" || data.none.name === "none"){
-    indentsBody.push(<img alt="body" src={parts.parts[1].src} width="74" height="106" onClick={function(event){changeImg(1, "body")}}></img>);
-    indentsBody.push(<img alt="body" src={parts.parts[2].src} width="74" height="106" onClick={function(event){changeImg(2, "body")}}></img>);
-    indentsBody.push(<img alt="body" src={parts.parts[3].src} width="74" height="106" onClick={function(event){changeImg(3, "body")}}></img>);
-    
-    indentsNeck.push(<img alt="neck" src={parts.parts[4].src} width="30" height="90" onClick={function(event){changeImg(4, "neck")}}></img>)
-    
-    indentsPeg.push(<img alt="peg" src={parts.parts[5].src} width="40" height="80" onClick={function(event){changeImg(5, "peg")}}></img>);
-    indentsPeg.push(<img alt="peg" src={parts.parts[6].src} width="40" height="80" onClick={function(event){changeImg(6, "peg")}}></img>);
-    indentsPeg.push(<img alt="peg" src={parts.parts[7].src} width="40" height="80" onClick={function(event){changeImg(7, "peg")}}></img>);
-    indentsPeg.push(<img alt="peg" src={parts.parts[8].src} width="40" height="80" onClick={function(event){changeImg(8, "peg")}}></img>);
-    console.log("indents");
+    indentsBody.push(<img alt="body" src={parts.parts[1].src} width="106" height="74" onClick={function(event){changeImg(1, "body")}}></img>);
+    indentsBody.push(<img alt="body" src={parts.parts[2].src} width="106" height="74" onClick={function(event){changeImg(2, "body")}}></img>);
+    indentsBody.push(<img alt="body" src={parts.parts[3].src} width="106" height="74" onClick={function(event){changeImg(3, "body")}}></img>);
+    indentsNeck.push(<img alt="neck" src={parts.parts[4].src} width="90" height="30" onClick={function(event){changeImg(4, "neck")}}></img>);
+    indentsPeg.push(<img alt="peg" src={parts.parts[5].src} width="80" height="40" onClick={function(event){changeImg(5, "peg")}}></img>);
+    indentsPeg.push(<img alt="peg" src={parts.parts[6].src} width="80" height="40" onClick={function(event){changeImg(6, "peg")}}></img>);
+    indentsPeg.push(<img alt="peg" src={parts.parts[7].src} width="80" height="40" onClick={function(event){changeImg(7, "peg")}}></img>);
+    indentsPeg.push(<img alt="peg" src={parts.parts[8].src} width="80" height="40" onClick={function(event){changeImg(8, "peg")}}></img>);
+    indentsNeck.push(<img alt="neck" src={parts.parts[9].src} width="90" height="30" onClick={function(event){changeImg(9, "neck")}}></img>);
     }
 }
 
-function downloadImage(){
-    var button = document.getElementById('btn-download');
-    button.addEventListener('click', function (e) {
-    var dataURL = canvas.toDataURL('image/png');
-    button.href = dataURL;
-});
-}
+    function startKonva(){
+    var count = 1;
+    var stage;
+    function drawImage(imageObj, lastimg) {
+            stage = new window.Konva.Stage({
+            container: 'container',
+            width: Math.max(window.innerWidth/2, 290),
+            height: Math.max(window.innerHeight/2, 400)
+        });
+
+        var layer = new window.Konva.Layer();
+        // darth vader
+        var bodyImg2 = new window.Konva.Image({
+            image: imgBody,
+            x: 300,
+            y: stage.getHeight() / 2 - imgBody.height / 4,
+            width: imgBody.width/2,
+            height: imgBody.height/2,
+            draggable: true
+        });
+        // add cursor styling
+        bodyImg2.on('mouseover', function() {
+            document.body.style.cursor = 'pointer';
+        });
+        bodyImg2.on('mouseout', function() {
+            document.body.style.cursor = 'default';
+        });
+        var neckImg2 = new window.Konva.Image({
+            image: imgNeck,
+            x: 170,
+            y: stage.getHeight() / 2 - imgNeck.height / 4,
+            width: imgNeck.width/2,
+            height: imgNeck.height/2,
+            draggable: true
+        });
+        // add cursor styling
+        neckImg2.on('mouseover', function() {
+            document.body.style.cursor = 'pointer';
+        });
+        neckImg2.on('mouseout', function() {
+            document.body.style.cursor = 'default';
+        });
+        var pegImg2 = new window.Konva.Image({
+            image: imgPeg,
+            x: 110,
+            y: stage.getHeight() / 2 - imgPeg.height / 4,
+            width: imgPeg.width/2,
+            height: imgPeg.height/2,
+            draggable: true
+        });
+        // add cursor styling
+        pegImg2.on('mouseover', function() {
+            document.body.style.cursor = 'pointer';
+        });
+        pegImg2.on('mouseout', function() {
+            document.body.style.cursor = 'default';
+        });
+
+        /*Activa los filtros de color*/
+        bodyImg2.cache();
+        neckImg2.cache();
+        pegImg2.cache();
+        bodyImg2.filters([window.Konva.Filters.HSV]);
+        neckImg2.filters([window.Konva.Filters.HSV]);
+        pegImg2.filters([window.Konva.Filters.HSV]);
+
+        layer.add(bodyImg2);
+        layer.add(neckImg2);
+        layer.add(pegImg2);
+        stage.add(layer);
+        
+            /*Guarda la imagen a local*/
+        function downloadURI(uri, name) {
+            var link = document.createElement("a");
+            link.download = name;
+            link.href = uri;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+        if(count === 3){
+        document.getElementById('btn-download').addEventListener('click', function () {
+            var dataURL = stage.toDataURL();
+            downloadURI(dataURL, 'construment-instrument.png');
+        }, false);
+        
+        document.getElementById('changeBody').addEventListener('click', function () {
+            imageObj3.src = imgBody.src;
+        }, false);
+        document.getElementById('changeNeck').addEventListener('click', function () {
+            imageObj2.src = imgNeck.src;
+        }, false);
+        document.getElementById('changePeg').addEventListener('click', function () {
+            imageObj1.src = imgPeg.src;
+        }, false);
+         count = count+1;
+        } else {
+            count = count+1;
+        }
+        
+        /*Asigna los cambios de color a las barras de desplazamiento*/
+        var sliders = ['hue', 'saturation', 'value'];
+        sliders.forEach(function (attr) {
+          var slider = document.getElementById(attr);
+          function update() {
+            bodyImg2[attr](parseFloat(slider.value));
+            neckImg2[attr](parseFloat(slider.value));
+            pegImg2[attr](parseFloat(slider.value));
+            layer.batchDraw();
+          }
+          slider.oninput = update;
+          update();
+        });
+    }
+    
+    var imageObj1 = new window.Image();
+    var imageObj2 = new window.Image();
+    var imageObj3 = new window.Image();
+    
+    imageObj1.onload = function() {
+        drawImage(this, false);  
+    };
+    imageObj2.onload = function() {
+        drawImage(this, false);  
+    };
+    imageObj3.onload = function() {
+        drawImage(this, false);  
+    };
+    
+    
+    imageObj1.src = imgPeg.src;
+    imageObj2.src = imgNeck.src;
+    imageObj3.src = imgBody.src;
+        
+    }
+
 
 class CanvasBuild extends Component {
 componentDidMount() {
     start();
-    downloadImage();
     clearContent();
+    startKonva();
 }
 
 componentWillMount() {
@@ -219,52 +224,47 @@ componentWillMount() {
             <div className="row">
             <div className="col-xs-12 col-md-5">
                 <button className="accordion">
-                <i class="fa fa-chevron-circle-down"></i> Body</button>
-                <div className="panel">
-                
+                <i className="fa fa-chevron-down"></i> Body</button>
+                <div id="changeBody" className="panel">
+
                 {indentsBody}
-                
+
                 </div>
                 <button className="accordion">
-                <i class="fa fa-chevron-circle-down"></i> Neck</button>
-                <div className="panel">
-                
+                <i className="fa fa-chevron-down"></i> Neck</button>
+                <div id="changeNeck" className="panel">
+
                 {indentsNeck}
-                
+
                 </div>
                 <button className="accordion">
-                <i class="fa fa-chevron-circle-down"></i> Peg</button>
-                <div className="panel">
-                
+                <i className="fa fa-chevron-down"></i> Peg</button>
+                <div id="changePeg" className="panel">
+
                 {indentsPeg}
-                
+
                 </div>
-                <a href="" id="btn-download" download="instrument-construment.png"><button className="accordion"><i class="fa fa-floppy-o"></i> Download</button>
+                <button id="btn-download" className="accordion"><i className="fa fa-download"></i> Download</button>
                 <div className="panel">
                 </div>
-                </a>
+                <button onClick={()=>{audioinstrumento()}} className="accordion" > <i className="fa fa-music"></i> Play {data.none.name}</button>
+                <div className="panel">
+                </div>
             </div>
 
-            <div className="col-xs-12 col-md-7">
-
-                
-                <canvas id="myCanvas" width="505" height="280" style={canvasStyle}> </canvas>
-                <img style={hideStyle} id="imgBody" width="50%" height="50%" src={parts.parts[1].src} alt="The Scream"/>
-                <img style={hideStyle} id="imgNeck" width="50%" height="50%" src={parts.parts[4].src} alt="The Scream"/>
-                <img style={hideStyle} id="imgPeg" width="50%" height="50%" src={parts.parts[5].src} alt="The Scream"/>
-                
-                <br/>
-                    <div class="btn-group">
-                    <button onClick={()=>{audioinstrumento()}} type="button" class="btn btn-dark"><i class="fa fa-music"></i> Play {data.none.name}</button>
-                    <button className="btn btn-success" onClick={downX} onMouseUp={addImage}><i class="fa fa-chevron-left"></i></button>
-                    <button className="btn btn-success" onClick={downY} onMouseUp={addImage}><i class="fa fa-chevron-up"></i></button>
-                    <button className="btn btn-success" onClick={upY} onMouseUp={addImage}><i class="fa fa-chevron-down"></i></button>
-                    <button className="btn btn-success" onClick={upX} onMouseUp={addImage}><i class="fa fa-chevron-right"></i></button>
-                    <button id="editChange" className="btn btn-default" onClick={changePart}>Move: {partEditing}</button>
-                    </div>
+            <div className="col-xs-12 col-md-7 ">
+            <div id="container" className="awa"></div>
+            <div id="controls">
+                Hue:
+                <input id="hue" type="range" min="0" max="259" step="1" defaultValue="0"/> Saturation:
+                <input id="saturation" type="range" min="-2" max="10" step="0.5" defaultValue="0"/> Lightness:
+                <input id="value" type="range" min="-2" max="2" step="0.1" defaultValue="0"/>
             </div>
             </div>
-
+                <img style={hideStyle} id="imgBody" src={parts.parts[1].src} alt="The Scream"/>
+                <img style={hideStyle} id="imgNeck" src={parts.parts[4].src} alt="The Scream"/>
+                <img style={hideStyle} id="imgPeg" src={parts.parts[5].src} alt="The Scream"/>
+            </div>
         </div>
         )//Fin del return
     };
--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -41,7 +41,7 @@ export default () => (
                 </Carousel>
             </div>
   </div>
-</section>       
+</section>
  </div>
  
 
